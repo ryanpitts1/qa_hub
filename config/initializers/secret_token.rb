f4892d6be30a6180a1af2af2d1a99c0ecbf44c52
Ryan Pitts <ryanpitts1@gmail.com>
2016-11-17 14:12:02 -0600

    Getting code up to date.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QaHub::Application.config.secret_token = '3dc6700148c95ae307ef15e637a232ab9bed082ab3d18b3e6eda9717953ef929f9be86b7d1e2bd76fbe1ef632b557db2912e0a6a6650ab14cc3613e7f9210a72'
